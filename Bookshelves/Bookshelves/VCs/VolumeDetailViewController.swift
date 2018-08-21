0024aeef8d298c8633146d3eae26007bf963f674
Samantha Gatt <smfgatt@gmail.com>
2018-08-21 17:44:28 -0400

    Creates VCs and updates storyboards

---
+++ Bookshelves/Bookshelves/VCs/VolumeDetailViewController.swift
@@ -0,0 +1,35 @@
+//
+//  VolumeDetailViewController.swift
+//  Bookshelves
+//
+//  Created by Samantha Gatt on 8/21/18.
+//  Copyright © 2018 Samantha Gatt. All rights reserved.
+//
+
+import UIKit
+
+class VolumeDetailViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ Bookshelves/Bookshelves/Views/VolumeCollectionViewCell.swift
@@ -0,0 +1,13 @@
+//
+//  VolumeCollectionViewCell.swift
+//  Bookshelves
+//
+//  Created by Samantha Gatt on 8/21/18.
+//  Copyright © 2018 Samantha Gatt. All rights reserved.
+//
+
+import UIKit
+
+class VolumeCollectionViewCell: UICollectionViewCell {
+    
+}
